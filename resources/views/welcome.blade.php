4fc60179bb4f979e21db4b4024721bc43d66d2bb
MarekPadd13 <markpdd13@list.ru>
2020-11-12 13:35:18 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Dep
+                    Laravel Dep sis
                 </div>
 
                 <div class="links">
